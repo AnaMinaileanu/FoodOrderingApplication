57bec80fc3c08fffa724a0b88852d7db99b4a579
MunteanuBianca <ioana.2013.bm@gmail.com>
2023-04-10 04:02:01 +0300

    Login activity

--- app/src/main/java/com/example/foodorderingapplication/WelcomeActivity.java
+++ app/src/main/java/com/example/foodorderingapplication/WelcomeActivity.java
@@ -22,4 +22,8 @@ public class WelcomeActivity extends AppCompatActivity {
     public void register(View view) {
         startActivity(new Intent(WelcomeActivity.this, RegistrationActivity.class));
     }
+
+    public void login(View view) {
+        startActivity(new Intent(WelcomeActivity.this, LoginActivity.class));
+    }
 }
