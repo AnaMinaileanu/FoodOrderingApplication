399741a9c53e5b9812f085a969fbf27f6d8bc486
MunteanuBianca <ioana.2013.bm@gmail.com>
2023-04-18 17:36:17 +0300

    Login with google

--- app/src/main/java/com/example/foodorderingapplication/activities/LoginActivity.java
+++ app/src/main/java/com/example/foodorderingapplication/activities/LoginActivity.java
@@ -1,24 +1,115 @@
 package com.example.foodorderingapplication.activities;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.IntentSenderRequest;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.content.SharedPreferences;
+import android.widget.ImageView;
 import android.widget.Toast;
+
 
 import com.example.foodorderingapplication.MainActivity;
 import com.example.foodorderingapplication.R;
+import com.google.android.gms.auth.api.identity.BeginSignInRequest;
+import com.google.android.gms.auth.api.identity.BeginSignInResult;
+import com.google.android.gms.auth.api.identity.Identity;
+import com.google.android.gms.auth.api.identity.SignInClient;
+import com.google.android.gms.auth.api.identity.SignInCredential;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class LoginActivity extends AppCompatActivity {
+
+    SignInClient oneTapClient;
+    BeginSignInRequest signInRequest;
+    private static final int REQ_ONE_TAP = 2;  // Can be any integer unique to the Activity.
+    private boolean showOneTapUI = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //login with google
+
+        final ImageView googleBtn = (ImageView) findViewById(R.id.google_btn);
+        oneTapClient = Identity.getSignInClient(this);
+        signInRequest = BeginSignInRequest.builder()
+                .setPasswordRequestOptions(BeginSignInRequest.PasswordRequestOptions.builder()
+                        .setSupported(true)
+                        .build())
+                .setGoogleIdTokenRequestOptions(BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                        .setSupported(true)
+                        // Your server's client ID, not your Android client ID.
+                        .setServerClientId(getString(R.string.web_client_id))
+                        // Only show accounts previously used to sign in.
+                        .setFilterByAuthorizedAccounts(false)
+                        .build())
+                // Automatically sign in when exactly one credential is retrieved.
+                .setAutoSelectEnabled(true)
+                .build();
+        ActivityResultLauncher<IntentSenderRequest> activityResultLauncher =
+                registerForActivityResult(new ActivityResultContracts.StartIntentSenderForResult(), new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if(result.getResultCode() == Activity.RESULT_OK){
+                            try {
+                                SignInCredential credential = oneTapClient.getSignInCredentialFromIntent(result.getData());
+                                String idToken = credential.getGoogleIdToken();
+                                String username = credential.getId();
+                                String password = credential.getPassword();
+                                if (idToken !=  null) {
+                                    String email = credential.getId();
+                                    Toast.makeText(LoginActivity.this, "Email " +email, Toast.LENGTH_SHORT).show();
+                                    Intent displayScreen = new Intent(LoginActivity.this, MainActivity.class);
+                                    startActivity(displayScreen);
+                                } else if (password != null) {
+                                    // Got a saved username and password. Use them to authenticate
+                                    // with your backend.
+                                    Log.d("TAG", "Got password.");
+                                }
+                            } catch (ApiException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                });
+        googleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                oneTapClient.beginSignIn(signInRequest)
+                        .addOnSuccessListener(LoginActivity.this, new OnSuccessListener<BeginSignInResult>() {
+                            @Override
+                            public void onSuccess(BeginSignInResult result) {
+                                IntentSenderRequest inetentSenderRequest =
+                                        new IntentSenderRequest.Builder(result.getPendingIntent().getIntentSender()).build();
+                                activityResultLauncher.launch(inetentSenderRequest);
+                            }
+                        })
+                        .addOnFailureListener(LoginActivity.this, new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                // No saved credentials found. Launch the One Tap sign-up flow, or
+                                // do nothing and continue presenting the signed-out UI.
+                                Log.d("TAG", e.getLocalizedMessage());
+                            }
+                        });
+            }
+        });
 
         //Finding id's for each widget
         final EditText etEmail = (EditText) findViewById(R.id.email);
